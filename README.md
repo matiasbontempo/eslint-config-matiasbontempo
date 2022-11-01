bace9e526956d4fb702ca74c91cccaf82f6a4855
Matias Bontempo <matias.bontempo@gmail.com>
2022-11-01 19:28:41 -0300

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# eslint-config-matiasbontempo
+
+These are some rules I like and use in most of my personal projects.
+
+```json
+// .eslintrc.json
+{
+  "extends": ["matiasbontempo"]
+}
+```
---
+++ src/index.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    "browser": true,
+    "es2021": true
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "airbnb",
+    "airbnb-typescript"
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    "ecmaVersion": "latest",
+    "sourceType": "module",
+    "project": "./tsconfig.json"
+  },
+  plugins: [
+    "react",
+    "react-hooks",
+    "@typescript-eslint"
+  ],
+  rules: {
+    "react/function-component-definition": ["error", { "namedComponents": "arrow-function" }],
+    "react/jsx-key": "error",
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
+    "import/prefer-default-export": "off"
+  },
+};
